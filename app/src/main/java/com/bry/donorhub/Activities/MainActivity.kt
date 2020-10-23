b65447deb9a4a67f7d65ce35336ab11bd434c86b
Bry Onyoni <bry@Brys-MacBook-Pro.local>
2020-10-23 10:39:03 +0300

    Set up project and added sign up pages

---
+++ app/src/main/java/com/bry/donorhub/Activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bry.donorhub.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bry.donorhub.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
